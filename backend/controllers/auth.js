80b8311ff565cbdef63aae9a0d1aaf4bd8f38917
Dunvii <rsloik.arzur@gmail.com>
2022-10-09 12:56:06 -0300

    V1.0 | Add .env exemple file | Translate status in english.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -10,7 +10,7 @@ exports.signup = (req, res, next) => {
           password: hash
         });
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .then(() => res.status(201).json({ message: 'New user !' }))
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
@@ -20,12 +20,12 @@ exports.login = (req, res, next) => {
     Account.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Compte introuvable'});
+                return res.status(401).json({ message: 'No user found in database'});
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Mot de passe incorect.' });
+                        return res.status(401).json({ message: 'wrong password' });
                     }
                     res.status(200).json({
                         userId: user._id,
